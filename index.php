79c10efd86edfa2486e414400231914245168035
cedrick <cedrickchu123@gmail.com>
2023-09-20 13:39:18 +0800

    meni tsenges

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>Word Frequency Counter</title>
     <link rel="stylesheet" type="text/css" href="styles.css">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
     <h1>Word Frequency Counter</h1>
@@ -21,7 +21,6 @@
         
         <label for="limit">Number of words to display:</label>
         <input type="number" id="limit" name="limit" value="10" min="1"><br><br>
-        
         <input type="submit" value="Calculate Word Frequency">
     </form>
 </body>
